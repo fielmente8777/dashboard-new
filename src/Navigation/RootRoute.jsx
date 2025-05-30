f76101ad8c2b57cb5a182f7dd7e200fc40b8816b
Hem Bhadur <sushilkc2611@gmail.com>
2025-05-30 17:19:21 +0530

    add cookies functionality add

--- src/Navigation/RootRoute.jsx
+++ src/Navigation/RootRoute.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { BASE_PATH } from "../data/constant";
 import { useEffect } from "react";
 
--- src/Protected/ProtecRoute.jsx
+++ src/Protected/ProtecRoute.jsx
@@ -1,17 +1,25 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate, useParams } from "react-router-dom";
 import DashboardLayout from "../layout/DashboardLayout";
 import handleLocalStorage from "../utils/handleLocalStorage";
+import { useEffect } from "react";
+import { getCookie } from "../utils/handleCookies";
 
 const isAuthenticated = () => {
-  const isToken = handleLocalStorage("token");
-
-  console.log(isToken);
-
+  const isToken = getCookie("token");
   if (isToken) return true;
   return false;
 };
 
 export default function ProtectedRoute() {
+  const hid = handleLocalStorage("hid");
+  const { ndid } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (hid) {
+      if (ndid !== String(handleLocalStorage("hid"))) navigate("/login");
+    }
+  }, [ndid]);
   return isAuthenticated() ? (
     <DashboardLayout>
       <Outlet />
