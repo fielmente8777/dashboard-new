b60a0c5e3d45b520cc08f67d67619d9cb0652356
abhijeet <abhijeet@eazotel.com>
2025-06-10 17:20:00 +0530

    add export data into excel

--- src/pages/Enquiry/Leads.jsx
+++ src/pages/Enquiry/Leads.jsx
@@ -6,7 +6,8 @@ import { formatDateTime } from "../../services/formateDate";
 import handleLocalStorage from "../../utils/handleLocalStorage";
 import { getAllClientEnquires } from "../../services/api/clientEnquire.api";
 import FilterPopup from "../../components/Popup/FilterPopup";
-
+import { FaFileExcel } from "react-icons/fa";
+import jsonToCsvExport from "json-to-csv-export";
 export const extractBookingInfo = (input) => {
   if (!input) return null;
   const parts = input.split(",");
@@ -29,6 +30,7 @@ export const extractBookingInfo = (input) => {
 const Leads = () => {
   const [active, setActive] = useState(0);
   const header = ["All Enquires", "Open Queries", "Contacted", "Converted"];
+  const [exportedData, setExportedData] = useState([])
   const [enquires, setEnquires] = useState([]);
   const [filteredEnquires, setFilteredEnquires] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -41,11 +43,31 @@ const Leads = () => {
 
   const [open, setOpen] = useState(false);
 
+  const createExportData = (apiData) => {
+    return apiData.map((item) => {
+      const message = item?.Message || "";
+
+      const checkInMatch = message.match(/check-in:\s*(\d{2}-\d{2}-\d{4})/i);
+      const checkOutMatch = message.match(/check-out:\s*(\d{2}-\d{2}-\d{4})/i);
+      const guestsMatch = message.match(/number of guest:\s*([\w\s\d]+)/i);
+      return {
+        ...item,
+        check_in: checkInMatch ? checkInMatch[1] : null,
+        check_out: checkOutMatch ? checkOutMatch[1] : null,
+        number_of_guest: guestsMatch ? guestsMatch[1].trim() : null,
+      };
+    })
+  }
+
+
+
   const fetchEnquires = async (token) => {
     setLoading(true);
     try {
       const hid = handleLocalStorage("hid");
       const response = await getAllClientEnquires({ token, hid });
+      const data = createExportData(response)
+      setExportedData(data)
       setEnquires(response?.reverse());
     } catch (error) {
       console.error("Error fetching enquires:", error);
@@ -128,6 +150,24 @@ const Leads = () => {
     }
   };
 
+
+
+
+
+
+
+  const headers = [
+    { key: "Name", label: "Name" },
+    { key: "Contact", label: "Contact" },
+    { key: "Email", label: "Email" },
+    { key: "check_in", label: "Check In" },
+    { key: "check_out", label: "Check Out" },
+    { key: "number_of_guest", label: "Number of Guest" },
+    { key: "Message", label: "Message" },
+    { key: "created_from", label: "Lead Source" },
+    { key: "status", label: "Status" },
+  ]
+
   return (
     <div className="cardShadow">
       <div className="flex justify-between items-center bg-white">
@@ -136,25 +176,24 @@ const Leads = () => {
             <button
               onClick={() => handleTabClick(index)}
               key={index}
-              className={`text-[14px] ${
-                active === index
-                  ? "border-b-2 border-[#575757]"
-                  : "border-b-2 border-transparent"
-              } px-4 py-3 bg-white font-medium text-[#575757]`}
+              className={`text-[14px] ${active === index
+                ? "border-b-2 border-[#575757]"
+                : "border-b-2 border-transparent"
+                } px-4 py-3 bg-white font-medium text-[#575757]`}
             >
               {item}
             </button>
           ))}
           <div
             onClick={() => fetchEnquires(localStorage.getItem("token"))}
-            className={`flex justify-end items-center text-[#575757] px-3 cursor-pointer ${
-              loading ? "animate-spin" : ""
-            } `}
+            className={`flex justify-end items-center text-[#575757] px-3 cursor-pointer ${loading ? "animate-spin" : ""
+              } `}
           >
             <MdRefresh size={25} />
           </div>
         </div>
         <div className=" py-2 px-4 mt-4 flex items-center gap-4">
+
           <label
             htmlFor="itemsPerPage"
             className="text-sm font-medium text-gray-700"
@@ -173,6 +212,10 @@ const Leads = () => {
             <option value={50}>50</option>
             <option value={100}>100</option>
           </select>
+
+          <div onClick={() => jsonToCsvExport({ data: exportedData, headers })} className="bg-green-500  text-white border py-1 px-3 cursor-pointer rounded flex items-center gap-2 ">
+            <FaFileExcel /><span className="font-medium">Export</span>
+          </div>
         </div>
       </div>
 
@@ -231,11 +274,10 @@ const Leads = () => {
                   {currentItems.map((enquery, index) => (
                     <tr
                       key={index}
-                      className={`py-1 border-b odd:bg-gray-50 even:bg-gray-100 border-gray-200 hover:bg-[#f8f8fb] transition duration-300 cursor-pointer ${
-                        enquery?.status === "Open"
-                          ? " text-[#575757]"
-                          : "text-[#575757]"
-                      }`}
+                      className={`py-1 border-b odd:bg-gray-50 even:bg-gray-100 border-gray-200 hover:bg-[#f8f8fb] transition duration-300 cursor-pointer ${enquery?.status === "Open"
+                        ? " text-[#575757]"
+                        : "text-[#575757]"
+                        }`}
                       onClick={() => {
                         setSelectedLead(enquery);
                         setIsPopupOpen(true);
@@ -268,13 +310,13 @@ const Leads = () => {
                         {enquery?.check_in
                           ? enquery.check_in
                           : extractBookingInfo(enquery?.Message)?.checkIn ||
-                            "-"}
+                          "-"}
                       </td>
                       <td className="py-3 px-2 text-[14px] text-[#575757]">
                         {enquery?.check_out
                           ? enquery.check_out
                           : extractBookingInfo(enquery?.Message)?.checkOut ||
-                            "-"}
+                          "-"}
                       </td>
                       <td className="py-3 px-2 text-[14px] text-[#575757]">
                         {enquery?.status}
@@ -312,11 +354,10 @@ const Leads = () => {
                 onClick={() => handlePageChange(currentPage - 1)}
                 disabled={currentPage === 1}
                 className={`px-3 py-1.5 text-sm rounded-md transition-all whitespace-nowrap duration-200
-          ${
-            currentPage === 1
-              ? "text-gray-300 cursor-not-allowed"
-              : "hover:bg-gray-100 text-gray-700"
-          }`}
+          ${currentPage === 1
+                    ? "text-gray-300 cursor-not-allowed"
+                    : "hover:bg-gray-100 text-gray-700"
+                  }`}
               >
                 ← Previous
               </button>
@@ -351,11 +392,10 @@ const Leads = () => {
                         key={item}
                         onClick={() => handlePageChange(item)}
                         className={`px-3 py-1.5 text-sm rounded-md font-medium transition-all duration-200
-                  ${
-                    currentPage === item
-                      ? "bg-primary text-white shadow-sm"
-                      : "text-gray-700 hover:bg-gray-100"
-                  }`}
+                  ${currentPage === item
+                            ? "bg-primary text-white shadow-sm"
+                            : "text-gray-700 hover:bg-gray-100"
+                          }`}
                       >
                         {item}
                       </button>
@@ -368,11 +408,10 @@ const Leads = () => {
                 onClick={() => handlePageChange(currentPage + 1)}
                 disabled={currentPage === totalPages}
                 className={`px-3 py-1.5 text-sm rounded-md whitespace-nowrap transition-all duration-200
-          ${
-            currentPage === totalPages
-              ? "text-gray-300 cursor-not-allowed"
-              : "hover:bg-gray-100 text-gray-700"
-          }`}
+          ${currentPage === totalPages
+                    ? "text-gray-300 cursor-not-allowed"
+                    : "hover:bg-gray-100 text-gray-700"
+                  }`}
               >
                 Next →
               </button>
@@ -389,7 +428,7 @@ const Leads = () => {
         handleTabClick={handleTabClick}
         activeIndex={active}
       />
-    </div>
+    </div >
   );
 };
 
