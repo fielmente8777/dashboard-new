ebc816b5811ffa06327b80cb142417b65b69e87d
abhijeet <abhijeet@eazotel.com>
2025-07-16 10:36:17 +0530

    up

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -136,32 +136,32 @@ const Login = () => {
   }
 
 
-  const handleSuccess = async (response) => {
-    setLoading(true);
-    setError(null);
-    try {
-      const token = response.credential;
-      console.log(token)
+  // const handleSuccess = async (response) => {
+  //   setLoading(true);
+  //   setError(null);
+  //   try {
+  //     const token = response.credential;
+  //     console.log(token)
 
-      // const jwtToken = (await verify(token)).data.jwt_token;
-      // console.log(jwtToken)
+  //     // const jwtToken = (await verify(token)).data.jwt_token;
+  //     // console.log(jwtToken)
 
-      // await testprotected(jwtToken);
-      // saveToken(jwtToken);
-      // navigate("/onboarding");
-      return "Login successful";
-    } catch (err) {
-      console.error("Authentication error:", err);
-      setError("Failed to authenticate. Please try again.");
-    } finally {
-      setLoading(false);
-    }
-  };
+  //     // await testprotected(jwtToken);
+  //     // saveToken(jwtToken);
+  //     // navigate("/onboarding");
+  //     return "Login successful";
+  //   } catch (err) {
+  //     console.error("Authentication error:", err);
+  //     setError("Failed to authenticate. Please try again.");
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
-  const handleFailure = (error) => {
-    console.error("Login Failed:", error);
-    setError("Login failed. Please try again.");
-  };
+  // const handleFailure = (error) => {
+  //   console.error("Login Failed:", error);
+  //   setError("Login failed. Please try again.");
+  // };
 
   return (
     <div className="w-full h-screen flex items-center justify-center md:px-6">
