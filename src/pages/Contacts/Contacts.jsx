042bd52633d6611d02e75e9b0bd48f2936a99e8b
abhijeet <abhijeetsingh5631@gmail.com>
2025-11-26 15:58:40 +0530

    update contact page

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,68 +1,16 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "../../data/constant";
-import { formatDate, formatDateByDay } from "../../utils/formateData";
+import { formatDateByDay } from "../../utils/formateData";
 import AddContactPopup from "../../components/Popup/AddContactPopup";
-
-const dummyContacts = [
-  {
-    id: 1,
-    name: "John Doe",
-    contact: "9800000001",
-    email: "john@example.com",
-    guests: 2,
-    checkIn: "2025-12-20",
-    checkOut: "2025-12-22",
-    stage: "Open",
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    contact: "9800000002",
-    email: "jane@example.com",
-    guests: 4,
-    checkIn: "-",
-    checkOut: "-",
-    stage: "Potential",
-  },
-  {
-    id: 3,
-    name: "David Lee",
-    contact: "9800000003",
-    email: "david@example.com",
-    guests: 3,
-    checkIn: "2025-12-12",
-    checkOut: "2025-12-14",
-    stage: "Contacted",
-  },
-  {
-    id: 4,
-    name: "Emily Clark",
-    contact: "9800000004",
-    email: "emily@example.com",
-    guests: "-",
-    checkIn: "-",
-    checkOut: "-",
-    stage: "Open",
-  },
-  {
-    id: 5,
-    name: "Robert Fox",
-    contact: "9800000005",
-    email: "robert@example.com",
-    guests: 6,
-    checkIn: "2025-12-01",
-    checkOut: "2025-12-05",
-    stage: "Converted",
-  },
-];
-
+import {MdDelete} from "react-icons/md"
 const Contacts = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
 
   const [contacts, setContacts] = useState([]);
+  const [selectedContact,setSelectedContact]=useState({})
 
+  const[isEdit,setIsEdit]=useState(false);
   
   const getContacts= async() => {
      // API call to fetch contacts will be here
@@ -83,6 +31,7 @@ const Contacts = () => {
     }
   }
 
+
   useEffect(() => {
     getContacts();
   }, []);
@@ -94,21 +43,50 @@ const Contacts = () => {
 
 
   const [open,setOpen]=useState(false);
-  const handlePopup = () => {
-    // Logic to open AddContactPopup will be here
+  const handlePopup = (contact) => {
+    setIsEdit(true)
+    setSelectedContact(contact)
     setOpen(true);
-    console.log("Open Add Contact Popup");
+  }
+  const handleAddPopup=()=>{
+    setIsEdit(false)
+    setOpen(true);
+  }
+
+  const[loading,setLoading]=useState(false)
+
+  const handleDeleteContact=async(id)=>{
+    setLoading(true)
+    try {
+        const response= await fetch( `${BASE_URL}/contact/${id}`,{
+          method:"DELETE",
+          headers: {  
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${localStorage.getItem('token')}`
+          },
+        })
+
+        const result=await response.json();
+
+        if(result.Status===true){
+          getContacts()
+        }
+    } catch (error) {
+        console.log("Error:",error)
+    }finally{
+      setLoading(false);
+    }
   }
 
   return (
     <div className="p-2">
       <div className="flex justify-between items-center mb-2">
       <h2 className="text-xl font-semibold mb-4">Contacts</h2>
-      <button className="bg-primary py-2 px-5 text-white font-semibold" onClick={handlePopup}>Add new Contact</button>
+      <button className="bg-primary py-2 px-5 text-white font-semibold" onClick={handleAddPopup}>Add new Contact</button>
 
       </div>
      
-      <table className="w-full table-fixed text-left bg-[#0a3a75] text-white/90 rounded-sm shadow-sm">
+      <table className="w-full  text-left bg-[#0a3a75] text-white/90 rounded-sm shadow-sm">
         <thead>
           <tr className="border-b">
             {/* <th className="py-3 px-2 text-[14px] font-medium">Select</th> */}
@@ -119,6 +97,7 @@ const Contacts = () => {
             <th className="py-3 px-2 text-[14px] font-medium">Name</th>
             <th className="py-3 px-2 text-[14px] font-medium">Contact</th>
             <th className="py-3 px-2 text-[14px] font-medium">Email</th>
+            <th className="py-3 px-2 text-[14px] font-medium">Action</th>
           </tr>
         </thead>
 
@@ -127,19 +106,28 @@ const Contacts = () => {
             currentItems.map((row,index) => (
               <tr
                 key={row.id}
+                onClick={()=>handlePopup(row)}
                 className="py-1 border-b odd:bg-gray-50 even:bg-gray-100 text-black"
               >
                 {/* <td className="py-3 px-2">
                   <input type="checkbox" />
                 </td> */}
                 <td className=" flex-1 py-3 px-2">{index+1}</td>
-                <td className=" flex-1 py-3 px-2">{row.added_from}</td>
 
                 <td className=" flex-1 py-3 px-2">{formatDateByDay(row.created_at)}</td>
+                <td className=" flex-1 py-3 px-2">{row.added_from?.toLowerCase()==="eazobot"?"Eazbot":row.added_from}</td>
 
                 <td className="  flex-1 py-3 px-2 whitespace-nowrap">{row?.name}</td>
                 <td className=" flex-1 py-3 px-2">{row.phone}</td>
                 <td className=" flex-1 py-3 px-2">{row.email}</td>
+                <td >
+                  <button 
+                  onClick={(e)=>{e.stopPropagation(); handleDeleteContact(row._id)}} 
+                  className={`border hover:bg-red-600 hover:text-white flex justify-center text-lg  text-red-600 cursor-pointer flex-1 py-2 px-2`}>
+                  <MdDelete />
+
+                  </button>
+                  </td>
               </tr>
             ))
           ) : (
@@ -176,7 +164,7 @@ const Contacts = () => {
       </div>
 
 
-      <div><AddContactPopup open={open} setOpen={setOpen} getContacts={getContacts} /></div>
+      <div><AddContactPopup open={open} setOpen={setOpen} isEdit={isEdit} contact={selectedContact} setSelectedContact={setSelectedContact} getContacts={getContacts} /></div>
     </div>
   );
 };
