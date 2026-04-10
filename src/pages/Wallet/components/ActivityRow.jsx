5aea312d9f55a3340ff50bd010576b8cda5e97dc
kajal021 <kajal@eazotel.com>
2026-04-10 18:33:20 +0530

    added eazowallet

--- src/pages/Wallet/components/ActivityRow.jsx
+++ src/pages/Wallet/components/ActivityRow.jsx
@@ -3,7 +3,23 @@ const ActivityRow = ({ date,service, action, credits, status}) => {
 return (
     <tr className="border-b">
         <td className="py-2">{date ||"_"}</td>
-        <td>{service || "_"}</td>
+        <td>
+  <span
+    className={`px-2 py-1 rounded-full text-xs font-medium ${
+      service === "AI"
+        ? "bg-purple-100 text-purple-600"
+        : service === "WhatsApp"
+        ? "bg-green-100 text-green-600"
+        : service === "Email"
+        ? "bg-yellow-100 text-yellow-600"
+        : service === "Campaigns"
+        ? "bg-pink-100 text-pink-600"
+        : "bg-gray-100 text-gray-600"
+    }`}
+  >
+    {service}
+  </span>
+</td>
         <td>{action || "_"}</td>
         <td>{credits ?? 0}</td>
         <td>
