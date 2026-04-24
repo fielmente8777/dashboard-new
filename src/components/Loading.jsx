25f0ac1b71b5f32a405c170d6553948650ca07d4
Hem Bhadur <sushilkc2611@gmail.com>
2026-04-24 14:46:43 +0530

    changes done

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-background">
-                <div className="flex items-center gap-1 text-muted-foreground animate-pulse">
-                    <div>
-                        <img src="/public/ICON ORANGE.ico" alt="Loading" className="h-15 w-15" />
+      <div className="flex items-center gap-1 text-muted-foreground animate-pulse">
+        <div>
+          <img src="/ICON ORANGE.ico" alt="Loading" className="h-15 w-15" />
+        </div>
+        <p>Loading...</p>
+      </div>
+    </div>
+  );
+};
 
-                    </div>
-                    <p>Loading...</p>
-                </div>
-            </div>
-  )
-}
-
-export default Loading
+export default Loading;
