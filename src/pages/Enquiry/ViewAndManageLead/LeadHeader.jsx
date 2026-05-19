440f2c661c784a4574d84771b28594dfec17f7e0
abhijeet <abhijeetsingh5631@gmail.com>
2026-05-19 13:00:29 +0530

    update

--- src/pages/Enquiry/ViewAndManageLead/LeadHeader.jsx
+++ src/pages/Enquiry/ViewAndManageLead/LeadHeader.jsx
@@ -1,8 +1,12 @@
 const LeadHeader = ({ lead }) => (
-  <div className="bg-white rounded-lg  p-5">
+  <div className="bg-white rounded-lg  p-5 flex justify-between">
     <h1 className="text-2xl font-semibold text-gray-800 capitalize">
       {lead?.Name || "Lead"}
     </h1>
+
+    <button className="bg-orange-600 px-3 py-1 rounded-md text-sm font-medium! text-white">
+      Convert
+    </button>
     {/* <p className="text-sm text-gray-500 mt-1">Lead ID: {lead?._id}</p> */}
   </div>
 );
